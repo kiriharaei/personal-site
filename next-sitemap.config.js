480b6f9c27f4efca24fb26cf1b66a67d972f4a34
kiriharaei <kirihara.ei@gmail.com>
2024-06-30 20:08:03 +0900

    [feature]サイトマップ修正

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,15 @@
+const path = require('path');
+
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://www.akagi-eiji.net/', // 自分のサイトのURLに置き換え
-    generateRobotsTxt: true, // ロボットテキストファイルも生成する場合はtrue
-}
+  siteUrl: 'https://www.akagi-eiji.net/', // 自分のサイトのURLに置き換えてください
+  generateRobotsTxt: true, // ロボットテキストファイルも生成する場合はtrueに設定
+  exclude: [], // サイトマップから除外するパスを指定します
+  // その他の設定オプションを追加できます
+  pagesConfig: {
+    '/src/app/*': {
+      changefreq: 'daily', // 変更頻度を設定（任意）
+      priority: 0.7, // 優先度を設定（0.0から1.0の間、任意）
+    },
+    // 他のページの設定を必要に応じて追加
+  },
+};
